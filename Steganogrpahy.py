1a31a51e1c4c50d08de1f937e232efae9e8b52e0
LirakXhelili <121616011+LirakXhelili@users.noreply.github.com>
2023-04-15 21:38:01 +0200

    Update Steganogrpahy.py

--- Steganogrpahy.py
+++ Steganogrpahy.py
@@ -29,7 +29,25 @@ color_map = {
     "w": (0, 255, 128), # spring green
     "x": (128, 0, 255), # violet
     "y": (0, 255, 0), # lime green
-    "z": (128, 128, 0) # olive
+    "z": (128, 128, 0), # olive
+    "0": (255, 255, 255), # white
+    "1": (0, 0, 0), # black
+    "2": (255, 165, 0), # orange
+    "3": (255, 192, 203), # pink
+    "4": (46, 139, 87), # forest green
+    "5": (255, 215, 0), # gold
+    "6": (255, 69, 0), # orange red
+    "7": (218, 112, 214), # orchid
+    "8": (238, 232, 170), # pale goldenrod
+    "9": (0, 255, 255), # aqua
+    "!": (128, 0, 35), # maroon
+    "@": (0, 128, 4), # dark green
+    "#": (10, 0, 128), # navy
+    "$": (128, 123, 0), # olive
+    "%": (128, 5, 128), # purple
+    "^": (0, 138, 128), # teal
+    "&": (128, 108, 128), # gray
+    " ": (123, 128, 118), # space
 }
 
 
@@ -38,20 +56,38 @@ pad_color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 2
 
 # Encryption function
 def encrypt_text(text):
-    # Convert the text to lowercase and remove whitespace
-    text = text.lower().replace(" ", "")
+    # Convert the text to lowercase
+    text = text.lower()
 
     # Determine the dimensions of the image
     width = len(text)
     height = width
 
+    # Count the number of spaces in the text
+    num_spaces = text.count(" ")
+
+    # Increase the width and height to account for the spaces
+    width += num_spaces
+    height += num_spaces
+
     # Create a new image
     image = Image.new("RGB", (width, height), pad_color)
 
     # Map each character to a color and set the corresponding pixel
-    for i, char in enumerate(text):
-        color = color_map.get(char, pad_color)
-        image.putpixel((i, i), color)
+    x = 0
+    y = 0
+    for char in text:
+        if char == " ":
+            # Assign a color for spaces
+            color = color_map.get(" ", pad_color)
+            image.putpixel((x, y), color)
+            x += 1
+            y += 1
+        else:
+            color = color_map.get(char, pad_color)
+            image.putpixel((x, y), color)
+            x += 1
+            y += 1
 
     return image
 
@@ -64,12 +100,20 @@ def decrypt_text(image):
     colors = [image.getpixel((i, i)) for i in range(width)]
 
     # Map each color to a character and concatenate the results
-    text = "".join([next((char for char, color in color_map.items() if color == c), "") for c in colors])
+    text = ""
+    for c in colors:
+        if c == (123, 128, 118):
+            text += " "  # add a space for white pixels
+        else:
+            text += next((char for char, color in color_map.items() if color == c), "")
+
+
+    return text
 
     return text
 
 # Example usage
-plaintext = "DataSecurity"
+plaintext = "Leotrim2    34@@$!lirak"
 encrypted_image = encrypt_text(plaintext)
 decrypted_text = decrypt_text(encrypted_image)
 
